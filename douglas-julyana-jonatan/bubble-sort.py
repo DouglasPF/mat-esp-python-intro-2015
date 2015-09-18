c5630d4e1b70c13ca8c802527ae1f82ff6c42fbf
douglas parreira de freitas <douglaspfreitas@live.com>
2015-09-18 11:52:19 -0300

    Criei uma pasta douglas-julyana-jonatan e dentro dela um arquivo bubble-sort.py com as intruções do professor

---
+++ douglas-julyana-jonatan/bubble-sort.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+lista = [11, 18, 3, 1, 16, 12, 6, 19, 5, 0, 14, 4, 17, 9, 13, 7, 10, 15, 2, 8] #Valores da Lista
+N = 20 #Numero de Elementos
+for i in range(0, N-1, 1): #Andando do primeiro elemento até o penultimo de 1 em 1
+    for j in range(i+1, N, 1): #Andando do elemento seguinte a 'i' até o ultimo de 1 em 1
+        if lista[i] < lista[j]:
+            continue
+        else:
+            temp = lista[i] #Para inverter os valores
+            lista[i] = lista[j]
+            lista[j] = temp
+print(lista)
+
+#Para selecionar o primeiro elemento da lista 'lista[0]'
+#Para selecionar os cincos primeiros elementos da lista 'lista[0:4]'
+
+ 
+           
+            
+
+    
